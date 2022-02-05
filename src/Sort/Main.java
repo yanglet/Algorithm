545ca15a81b8508b7b55495093227474f24d2c8e
양충욱 <96788792+yanglet@users.noreply.github.com>
2022-02-05 14:07:34 +0900

    Add files via upload

---
+++ src/Sort/Main.java
@@ -0,0 +1,19 @@
+class BubbleSort {
+    public static void main(String args[]) {
+        int sort[] = {3, 6, 2, 1, 5, 9, 10, 8, 4, 7};
+        int temp;
+
+        for(int i=0; i<sort.length; i++){
+            for(int j=0; j<9-i; j++){
+                if(sort[j]>sort[j+1]){
+                    temp = sort[j];
+                    sort[j]=sort[j+1];
+                    sort[j+1]=temp;
+                }
+            }
+        }
+        for(int i=0; i<sort.length; i++){
+            System.out.println(sort[i]);
+        }
+    }
+}
