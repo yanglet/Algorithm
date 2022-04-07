b4fcf0d1dd07782198126cb71eabd6065a094a28
yanglet <y2yone023@naver.com>
2022-04-07 19:02:48 +0900

    해쉬

---
+++ src/Programmers/level1/완주하지못한선수.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        Map<String, Integer> map = new HashMap<>();
+        
+        for(String s : participant){
+            // 동명이인일 경우를 생각하기
+            map.put(s, map.getOrDefault(s, 0) + 1);
+        }
+        
+        // 0이 아닌 사람이 완주를 못한 것.
+        for(String s : completion){
+            map.put(s, map.get(s) - 1);
+        }
+        
+        for(String key : map.keySet()){
+            if(map.get(key) == 1){
+                answer += key;
+            }
+        }
+       
+        return answer;
+    }
+}
