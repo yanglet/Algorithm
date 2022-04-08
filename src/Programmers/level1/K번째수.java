f8c12082099650cfc30b3e10c7fa79aee364d70e
양충욱 <96788792+yanglet@users.noreply.github.com>
2022-04-09 02:07:45 +0900

    Create K번째수.java

---
+++ src/Programmers/level1/K번째수.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+        int n = 0;
+        
+        for(int[] a : commands){
+            int i = a[0];
+            int j = a[1];
+            int k = a[2];
+            
+            int[] sArr = Arrays.copyOfRange(array, i - 1, j);
+            
+            Arrays.sort(sArr);
+            
+            answer[n++] = sArr[k - 1];
+        }
+        
+        return answer;
+    }
+}
