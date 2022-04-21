414b6f20af92f55081323066187ebe91f3a97bea
양충욱 <96788792+yanglet@users.noreply.github.com>
2022-04-21 17:11:20 +0900

    Create 소수 만들기.java

---
+++ Programmers/level1/소수 만들기.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int solution(int[] nums) {
+        int answer = 0;
+
+        for(int i=0; i<nums.length; i++){
+            for(int j=i+1; j<nums.length; j++){
+                for(int k=j+1; k<nums.length; k++){
+                    int sum = 0;
+                    sum = nums[i] + nums[j] + nums[k];
+                    if( isPrime(sum) ){
+                        answer++;
+                    }
+                }
+            }
+        }
+
+        return answer;
+    }
+    
+    public boolean isPrime(int sum){
+        for(int i=2; i<sum; i++){
+            if(sum % i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
