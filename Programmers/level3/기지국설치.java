3cf6a7af919d3f0e720df8ca13617bdfccb949fd
양충욱 <96788792+yanglet@users.noreply.github.com>
2022-04-30 13:13:06 +0900

    Create 기지국설치.java

---
+++ Programmers/level3/기지국설치.java
@@ -0,0 +1,42 @@
+class Solution {
+    static int check = 0;
+    static int answer = 0;
+    public int solution(int n, int[] stations, int w) {
+        
+        for(int i=0; i<stations.length; i++){
+            if(check >= n){
+                break;
+            }
+            
+            int left = stations[i] - (w + 1);
+            int right = stations[i] + w;
+            int dist = left - check;
+            
+            if(dist > 0){
+                install(dist, stations[i], w);
+            }else{
+                check = stations[i] + w;
+            }
+            
+            if(i == stations.length - 1 && check < n){
+                dist = n - right;
+                install(dist, stations[i], w);
+            }
+        }
+
+        return answer;
+    }
+    
+    public static void install(int dist, int station, int w){
+        int a = dist / (w * 2 + 1);
+        int b = dist % (w * 2 + 1);
+        
+        if(b > 0){
+            answer += a + 1;
+            check = station + w;
+        }else{
+            answer += a;
+            check = station + w;
+        }
+    }
+}
